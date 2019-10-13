ce43fc32990d9d05f9f9c5750d3df2525bd65fe7
TomaszOrpik <orpik.tomasz@gmail.com>
2019-10-13 21:14:11 +0200

    tasks

--- 1_Zadania/obowiązkowe/6_constructor_function/js/app.js
+++ 1_Zadania/obowiązkowe/6_constructor_function/js/app.js
@@ -1,16 +1,18 @@
 //Zadanie #0
 console.log("Zadanie 0:");
 
-var Basket = {
+
+
+function Basket() {
     products = [],
     prices = [],
     sum = 0,
-    addProduct: function(name, number) {
+    this.addProduct = function(name, number) {
         products.push(name);
         prices.push(number);
         sum = sum + number;
     },
-    showBasket: function() {
+    this.showBasket = function() {
         for(var i=0; i<products.length; i++)
             console.log("produkt: "+products[i]+" cena: "+prices[i]);
 
@@ -18,10 +20,15 @@ var Basket = {
     }
 }
 
-//var aliceBasket = new Basket();
-//aliceBasket.addProduct("pomidor",10);
-
 var aliceBasket = new Basket();
 aliceBasket.addProduct("pomidor", 10);
+aliceBasket.addProduct("arbuz", 40);
 aliceBasket.showBasket();
+
+var bruceBasket = new Basket();
+bruceBasket.addProduct("rice", 10);
+bruceBasket.addProduct("grzyby mun", 50);
+bruceBasket.addProduct("tofu", 20);
+bruceBasket.showBasket();
+
 
